d78476daf41691f51324720cc4161eaef918362f
Samuel408 <samuelsaldivar11@gmail.com>
2023-11-13 23:12:34 -0800

    merged projects to main

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import NavBar from './component/NavBar';
 import About from './component/About';
+import Projects from './component/Projects';
 
 function App() {
   return (
     <div >
      <NavBar />
      <About/>
+     <Projects/>
     </div>
   );
 }

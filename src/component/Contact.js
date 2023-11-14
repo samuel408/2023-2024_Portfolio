e7ad309da2b7feeabf4e0e07f8c53caea7f9d31a
Samuel408 <samuelsaldivar11@gmail.com>
2023-11-13 23:27:25 -0800

    starting contact info page

---
+++ src/component/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+
+function Contact(){
+    const titleStyle = {
+        textAlign: 'center',
+    };
+    return (
+        <div>
+            <h2 style={titleStyle}>Contact</h2>
+        </div>
+
+    );  
+
+};
+
+
+export default Contact;

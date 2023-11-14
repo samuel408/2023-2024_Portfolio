93b4d53a70638c7c842fef2cf4b4f90b237c24ad
Samuel408 <samuelsaldivar11@gmail.com>
2023-11-13 21:58:16 -0800

    created about me section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from './component/NavBar';
+import About from './component/About';
 
 function App() {
   return (
     <div >
      <NavBar />
+     <About/>
     </div>
   );
 }
---
+++ src/component/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function About() {
+    const containerStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        backgroundColor: 'white',
+        color: 'white',
+        padding: '10px',
+        textAlign: 'right',
+    };
+
+    return (
+        <div className="containerStyle"> 
+
+        <h1>About Me</h1>
+
+        </div>
+    );
+
+}
+
+export default About;

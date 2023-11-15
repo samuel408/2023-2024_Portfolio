2750001519c8adb8d638e952c386b5111d7231eb
Samuel408 <samuelsaldivar11@gmail.com>
2023-11-15 00:41:42 -0800

    testing

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
+app.post(process.env.REACT_APP_SERVER_URL, (req, res) => {
     console.log('Received a POST request to /');
 
   const { name, email, message } = req.body;

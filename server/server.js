16529cda4bcf4d50f726178e569a123705d0dc49
Samuel408 <samuelsaldivar11@gmail.com>
2023-11-19 12:23:01 -0800

    testing react server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 50000;
 
 const corsOptions = {
-    origin:  process.env.REACT_APP_SERVER_URL||'https://sleepy-sierra-38019-875c43c0002d.herokuapp.com/',
+    origin:  process.env.REACT_APP_SERVER_URL||'https://papaya-kleicha-d44a7a.netlify.app/',
   };
   
   app.use(cors(corsOptions));
